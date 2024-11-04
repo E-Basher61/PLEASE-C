82e461bc90140e849cd5a35867ed31b862b7d78e
E-Basher61 <158016361+E-Basher61@users.noreply.github.com>
2024-11-04 21:50:43 +0800

    Update Info - 1. Personal Info Display Program

---
+++ Basic Concepts/Basic Declarations & Expressions/1. Personal Info Display Program.c
@@ -0,0 +1,15 @@
+/*
+- Personal Info Display Program
+- Print your name, date of birth, email, and mobile number.
+- Expected Output:
+
+----------------------------
+Name    : John Doe
+BDate   : Jan 1st, 2000
+EMail   : johndoe@gmail.com
+MNumber : 111-1111111
+----------------------------
+
+- The name, date of birth, and mobile number can be adjusted/modified to your liking. */
+    
+// Solution/Answer: Solution_1.c
