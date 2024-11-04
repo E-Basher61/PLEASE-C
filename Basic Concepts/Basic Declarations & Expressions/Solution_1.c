6e8827298fdaa8513b0834084e334fccb2b1a551
E-Basher61 <158016361+E-Basher61@users.noreply.github.com>
2024-11-04 21:53:20 +0800

    Create Solution_1.c

---
+++ Basic Concepts/Basic Declarations & Expressions/Solution_1.c
@@ -0,0 +1,18 @@
+// Including the "stdio.h" header file
+#include <stdio.h>
+
+// The main function
+int main()  
+{
+    // Printing the "Name"
+    printf("Name    : John Doe\n");
+    // Printing the "Date Of Birth"
+    printf("BDate   : Jan 1st, 2000\n");
+    // Printing the "E-Mail"
+    printf("EMail   : johndoe@gmail.com\n");
+	// Printing the "Mobile Number"
+    printf("MNumber : 111-1111111\n");
+
+	// Returning "0" If The Code Is Successfully Compiled & Executed
+    return(0); 
+}
